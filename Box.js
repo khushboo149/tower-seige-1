c1f90968b36fdd86a92ce73bd296fcb128ae2f83
khushboo149 <71179638+khushboo149@users.noreply.github.com>
2021-01-15 23:01:56 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            'restituion':0.8,
+            'friction': 1.0,
+    'density': 1.0,
+}
+this.Body = Bodies.rectangle(x,y,width,height,options);
+this.height = height;
+this.width=width;
+World.add(world,this.body);
+    }
+    display(){
+        var pos= this.body.position;
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+        fill("white");
+    }
+        }
+    
